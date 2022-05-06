9449e6ced47ddd18ee19959da714aa3b5ef5708f
Pheel_Eu <euclidi.filippo@gmail.com>
2022-05-06 11:36:04 +0200

    made a commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">E-COMMERCE</a>
+      </header>
+      <main>
+        Hello World
+      </main>
+    </div>
+  );
+}
+
+export default App;
