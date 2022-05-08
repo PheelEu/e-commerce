a61e18477795493bf246f2c24cbd89566a75d14f
Pheel_Eu <euclidi.filippo@gmail.com>
2022-05-08 17:18:48 +0200

    Implemented backend and frontend for data transfer

--- frontend/src/pages/product_page.js
+++ frontend/src/pages/product_page.js
@@ -2,10 +2,10 @@ import { useParams } from 'react-router-dom';
 
 function ProductPage() {
   const params = useParams();
-  const { slug } = params;
+  const { id } = params;
   return (
     <div>
-      <h1>{slug}</h1>
+      <h1>{id}</h1>
     </div>
   );
 }
