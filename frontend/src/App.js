eed6b24a2c3351303bc152e3d507339f87d37d7f
Pheel_Eu <euclidi.filippo@gmail.com>
2022-05-10 16:36:14 +0200

    implemented Cart Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/home_page";
-import ProductPage from "./pages/product_page";
-import {Navbar, Container}from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import CartPage from './pages/CartPage';
+import {Navbar, Container, Badge, Nav}from "react-bootstrap";
 import {LinkContainer} from 'react-router-bootstrap'
+import { useContext } from 'react';
+import { Store } from './Store';
+
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
+
   return (
     <Router>
       <div className="d-flex flex-column site-container">
@@ -15,13 +22,24 @@ function App() {
               <LinkContainer to="/">
                 <Navbar.Brand>E-COMMERCE</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                 <Link to="/cart" className="nav-link">
+                   Cart
+                   {cart.cartItems.length > 0 && (
+                     <Badge pill bg="danger">
+                         {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                     </Badge>
+                   )}
+                 </Link>
+               </Nav>
             </Container>
           </Navbar>
         </header>
         <main>
-          <Container>
+          <Container className="mt-3">
             <Routes>
-              <Route path="/product/:id" element={<ProductPage />} />
+              <Route path="/product/:slug" element={<ProductPage />} />
+              <Route path="/cart" element={<CartPage />} />
               <Route path="/" element={<HomePage />} />
             </Routes>
           </Container>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { HelmetProvider } from "react-helmet-async";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import { StoreProvider } from "./Store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StoreProvider>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </StoreProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
