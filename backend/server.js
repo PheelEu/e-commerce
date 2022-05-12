83f31f5f1727b9f27df31299c3b258304af4e65c
Pheel_Eu <euclidi.filippo@gmail.com>
2022-05-12 10:45:29 +0200

    implemented login

--- backend/server.js
+++ backend/server.js
@@ -1,62 +1,36 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from 'cors';
-import dotenv from 'dotenv';
-import seedRouter from './routes/seedRoutes.js';
-import productRouter from './routes/productRoutes.js';
-import userRouter from './routes/userRoutes.js';
+import express from 'express';
+import data from './data.js';
+import mongoose from 'mongoose';
+ import dotenv from 'dotenv';
+ import seedRouter from './routes/seedRoutes.js';
+ import productRouter from './routes/productRoutes.js';
+ import userRouter from './routes/userRoutes.js';
 
-dotenv.config();
-
-const port = process.env.PORT || 4000;
+ dotenv.config();
 
 mongoose
   .connect(process.env.DB_URI)
   .then(() => {
-    console.log('connected to DB');
+    console.log('connected to db');
   })
   .catch((err) => {
     console.log(err.message);
-  });
+   });
 
-const app = express();
+ const app = express();
 
-app.use(cors());
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
-app.use('/api/users', userRouter);
+ app.use('/api/seed', seedRouter);
+ app.use('/api/products', productRouter);
+ app.use('/api/users', userRouter);
 
  app.use((err, req, res, next) => {
    res.status(500).send({ message: err.message });
  });
 
-app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
+ const port = process.env.PORT || 5000;
+ app.listen(port, () => {
+  console.log(`Connected at http://localhost:${port}`);
 });
-
-/*
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRouter from "./routes/userRoutes.js";
-import orderRouter from "./routes/orderRoutes.js";
-import uploadRouter from "./routes/uploadRoutes.js";
-
-/*
-app.get("/api/keys/paypal", (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
-});
-app.get("/api/keys/google", (req, res) => {
-  res.send({ key: process.env.GOOGLE_API_KEY || "" });
-});
-
-app.use("/api/upload", uploadRouter);
-app.use("/api/users", userRouter);
-app.use("/api/orders", orderRouter);
-
-*/
-
-app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
