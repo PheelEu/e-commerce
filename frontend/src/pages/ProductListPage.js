125ea93933b68c38481b19f6c82830fdc1f24a2b
Pheel_Eu <euclidi.filippo@gmail.com>
2022-05-23 16:38:19 +0200

    Implemented User and Product list pages

--- frontend/src/pages/ProductListPage.js
+++ frontend/src/pages/ProductListPage.js
@@ -34,17 +34,42 @@ const reducer = (state, action) => {
     case "CREATE_FAIL":
       return { ...state, loadingCreate: false };
 
+    case "DELETE_REQUEST":
+      return { ...state, loadingDelete: true, successDelete: false };
+
+    case "DELETE_SUCCESS":
+      return {
+        ...state,
+        loadingDelete: false,
+        successDelete: true,
+      };
+    case "DELETE_FAIL":
+      return { ...state, loadingDelete: false, successDelete: false };
+
+    case "DELETE_RESET":
+      return { ...state, loadingDelete: false, successDelete: false };
+
     default:
       return state;
   }
 };
 
 export default function ProductListPage() {
-  const [{ loading, error, products, pages, loadingCreate }, dispatch] =
-    useReducer(reducer, {
-      loading: true,
-      error: "",
-    });
+  const [
+    {
+      loading,
+      error,
+      products,
+      pages,
+      loadingCreate,
+      loadingDelete,
+      successDelete,
+    },
+    dispatch,
+  ] = useReducer(reducer, {
+    loading: true,
+    error: "",
+  });
 
   const navigate = useNavigate();
   const { search } = useLocation();
@@ -64,27 +89,49 @@ export default function ProductListPage() {
         dispatch({ type: "FETCH_SUCCESS", payload: data });
       } catch (err) {}
     };
-    fetchData();
-  }, [page, userInfo]);
+
+    if (successDelete) {
+      dispatch({ type: "DELETE_RESET" });
+    } else {
+      fetchData();
+    }
+  }, [page, userInfo, successDelete]);
 
   const createHandler = async () => {
-    if (window.confirm('Are you sure to create?')) {
+    if (window.confirm("Are you sure to create?")) {
       try {
-        dispatch({ type: 'CREATE_REQUEST' });
+        dispatch({ type: "CREATE_REQUEST" });
         const { data } = await axios.post(
-          '/api/products',
+          "/api/products",
           {},
           {
             headers: { Authorization: `Bearer ${userInfo.token}` },
           }
         );
-        toast.success('product created successfully');
-        dispatch({ type: 'CREATE_SUCCESS' });
+        toast.success("product created successfully");
+        dispatch({ type: "CREATE_SUCCESS" });
         navigate(`/admin/product/${data.product._id}`);
       } catch (err) {
         toast.error(getError(error));
         dispatch({
-          type: 'CREATE_FAIL',
+          type: "CREATE_FAIL",
+        });
+      }
+    }
+  };
+
+  const deleteHandler = async (product) => {
+    if (window.confirm("Are you sure to delete?")) {
+      try {
+        await axios.delete(`/api/products/${product._id}`, {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        });
+        toast.success("product deleted successfully");
+        dispatch({ type: "DELETE_SUCCESS" });
+      } catch (err) {
+        toast.error(getError(error));
+        dispatch({
+          type: "DELETE_FAIL",
         });
       }
     }
@@ -94,19 +141,19 @@ export default function ProductListPage() {
     <div>
       <h1>Products</h1>
       <Row>
-         <Col>
-           <h1>Products</h1>
-         </Col>
-         <Col className="col text-end">
-           <div>
-             <Button type="button" onClick={createHandler}>
-               Create Product
-             </Button>
-           </div>
-         </Col>
-       </Row>
-
-       {loadingCreate && <LoadingBox></LoadingBox>}
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="col text-end">
+          <div>
+            <Button type="button" onClick={createHandler}>
+              Create Product
+            </Button>
+          </div>
+        </Col>
+      </Row>
+      {loadingCreate && <LoadingBox></LoadingBox>}
+      {loadingDelete && <LoadingBox></LoadingBox>}
       {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
@@ -133,14 +180,22 @@ export default function ProductListPage() {
                   <td>{product.category}</td>
                   <td>{product.brand}</td>
                   <td>
-                     <Button
-                       type="button"
-                       variant="dark"
-                       onClick={() => navigate(`/admin/product/${product._id}`)}
-                     >
-                       Edit
-                     </Button>
-                   </td>
+                    <Button
+                      type="button"
+                      variant="dark"
+                      onClick={() => navigate(`/admin/product/${product._id}`)}
+                    >
+                      Edit
+                    </Button>
+                    &nbsp;
+                    <Button
+                      type="button"
+                      variant="dark"
+                      onClick={() => deleteHandler(product)}
+                    >
+                      Delete
+                    </Button>
+                  </td>
                 </tr>
               ))}
             </tbody>
