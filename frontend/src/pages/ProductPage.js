9d53b00fd409f21d5658e5eac9c9d9199422eee9
Pheel_Eu <euclidi.filippo@gmail.com>
2022-05-25 16:17:06 +0200

    Upadated product page, and modified user edit page

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -45,7 +45,7 @@ function ProductPage() {
 
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState("");
-  const [selectedImage, setSelectedImage] = useState('');
+  const [selectedImage, setSelectedImage] = useState("");
 
   const navigate = useNavigate();
   const params = useParams();
@@ -91,7 +91,7 @@ function ProductPage() {
   const submitHandler = async (e) => {
     e.preventDefault();
     if (!comment || !rating) {
-      toast.error('Please enter comment and rating');
+      toast.error("Please enter comment and rating");
       return;
     }
     try {
@@ -104,20 +104,20 @@ function ProductPage() {
       );
 
       dispatch({
-        type: 'CREATE_SUCCESS',
+        type: "CREATE_SUCCESS",
       });
-      toast.success('Review submitted successfully');
+      toast.success("Review submitted successfully");
       product.reviews.unshift(data.review);
       product.numReviews = data.numReviews;
       product.rating = data.rating;
-      dispatch({ type: 'REFRESH_PRODUCT', payload: product });
+      dispatch({ type: "REFRESH_PRODUCT", payload: product });
       window.scrollTo({
-        behavior: 'smooth',
+        behavior: "smooth",
         top: reviewsRef.current.offsetTop,
       });
     } catch (error) {
       toast.error(getError(error));
-      dispatch({ type: 'CREATE_FAIL' });
+      dispatch({ type: "CREATE_FAIL" });
     }
   };
 
@@ -131,7 +131,7 @@ function ProductPage() {
         <Col md={6}>
           <img
             className="img-large"
-             src={selectedImage || product.image}
+            src={selectedImage || product.image}
             alt={product.name}
           ></img>
         </Col>
@@ -149,25 +149,25 @@ function ProductPage() {
                 numReviews={product.numReviews}
               ></Rating>
             </ListGroup.Item>
-            <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Price : €{product.price}</ListGroup.Item>
             <ListGroup.Item>
-               <Row xs={1} md={2} className="g-2">
-                 {[product.image, ...product.images].map((x) => (
-                   <Col key={x}>
-                     <Card>
-                       <Button
-                         className="thumbnail"
-                         type="button"
-                         variant="light"
-                         onClick={() => setSelectedImage(x)}
-                       >
-                         <Card.Img variant="top" src={x} alt="product" />
-                       </Button>
-                     </Card>
-                   </Col>
-                 ))}
-               </Row>
-             </ListGroup.Item>
+              <Row xs={1} md={2} className="g-2">
+                {[product.image, ...product.images].map((x) => (
+                  <Col key={x}>
+                    <Card>
+                      <Button
+                        className="thumbnail"
+                        type="button"
+                        variant="light"
+                        onClick={() => setSelectedImage(x)}
+                      >
+                        <Card.Img variant="top" src={x} alt="product" />
+                      </Button>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </ListGroup.Item>
             <ListGroup.Item>
               Description:
               <p>{product.description}</p>
@@ -181,7 +181,7 @@ function ProductPage() {
                 <ListGroup.Item>
                   <Row>
                     <Col>Price:</Col>
-                    <Col>${product.price}</Col>
+                    <Col>€{product.price}</Col>
                   </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
@@ -212,72 +212,72 @@ function ProductPage() {
         </Col>
       </Row>
       <div className="my-3">
-         <h2 ref={reviewsRef}>Reviews</h2>
-         <div className="mb-3">
-           {product.reviews.length === 0 && (
-             <MessageBox>There is no review</MessageBox>
-           )}
-         </div>
-         <ListGroup>
-           {product.reviews.map((review) => (
-             <ListGroup.Item key={review._id}>
-               <strong>{review.name}</strong>
-               <Rating rating={review.rating} caption=" "></Rating>
-               <p>{review.createdAt.substring(0, 10)}</p>
-               <p>{review.comment}</p>
-             </ListGroup.Item>
-           ))}
-         </ListGroup>
-         <div className="my-3">
-           {userInfo ? (
-             <form onSubmit={submitHandler}>
-               <h2>Write a customer review</h2>
-               <Form.Group className="mb-3" controlId="rating">
-                 <Form.Label>Rating</Form.Label>
-                 <Form.Select
-                   aria-label="Rating"
-                   value={rating}
-                   onChange={(e) => setRating(e.target.value)}
-                 >
-                   <option value="">Select...</option>
-                   <option value="1">1- Poor</option>
-                   <option value="2">2- Fair</option>
-                   <option value="3">3- Good</option>
-                   <option value="4">4- Very good</option>
-                   <option value="5">5- Excelent</option>
-                 </Form.Select>
-               </Form.Group>
-               <FloatingLabel
-                 controlId="floatingTextarea"
-                 label="Comments"
-                 className="mb-3"
-               >
-                 <Form.Control
-                   as="textarea"
-                   placeholder="Leave a comment here"
-                   value={comment}
-                   onChange={(e) => setComment(e.target.value)}
-                 />
-               </FloatingLabel>
+        <h2 ref={reviewsRef}>Reviews</h2>
+        <div className="mb-3">
+          {product.reviews.length === 0 && (
+            <MessageBox>There is no review</MessageBox>
+          )}
+        </div>
+        <ListGroup>
+          {product.reviews.map((review) => (
+            <ListGroup.Item key={review._id}>
+              <strong>{review.name}</strong>
+              <Rating rating={review.rating} caption=" "></Rating>
+              <p>{review.createdAt.substring(0, 10)}</p>
+              <p>{review.comment}</p>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+        <div className="my-3">
+          {userInfo ? (
+            <form onSubmit={submitHandler}>
+              <h2>Write a customer review</h2>
+              <Form.Group className="mb-3" controlId="rating">
+                <Form.Label>Rating</Form.Label>
+                <Form.Select
+                  aria-label="Rating"
+                  value={rating}
+                  onChange={(e) => setRating(e.target.value)}
+                >
+                  <option value="">Select...</option>
+                  <option value="1">1- Poor</option>
+                  <option value="2">2- Fair</option>
+                  <option value="3">3- Good</option>
+                  <option value="4">4- Very good</option>
+                  <option value="5">5- Excelent</option>
+                </Form.Select>
+              </Form.Group>
+              <FloatingLabel
+                controlId="floatingTextarea"
+                label="Comments"
+                className="mb-3"
+              >
+                <Form.Control
+                  as="textarea"
+                  placeholder="Leave a comment here"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                />
+              </FloatingLabel>
 
-               <div className="mb-3">
-                 <Button disabled={loadingCreateReview} type="submit">
-                   Submit
-                 </Button>
-                 {loadingCreateReview && <LoadingBox></LoadingBox>}
-               </div>
-             </form>
-           ) : (
-             <MessageBox>
-               Please{' '}
-               <Link to={`/signin?redirect=/product/${product.slug}`}>
-                 Sign In
-               </Link>{' '}
-               to write a review
-             </MessageBox>
-           )}
-         </div>
-       </div>
+              <div className="mb-3">
+                <Button disabled={loadingCreateReview} type="submit">
+                  Submit
+                </Button>
+                {loadingCreateReview && <LoadingBox></LoadingBox>}
+              </div>
+            </form>
+          ) : (
+            <MessageBox>
+              Please{" "}
+              <Link to={`/signin?redirect=/product/${product.slug}`}>
+                Sign In
+              </Link>{" "}
+              to write a review
+            </MessageBox>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
